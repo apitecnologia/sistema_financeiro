8fa148441b9fe76d4ba5fa470d7e4c1a887ff61f
apitecnologia <victor_guerrero_sk8@hotmail.com>
2025-10-01 14:57:09 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
-# build.sh
-
-# Sair imediatamente se um comando falhar
+# exit on error
 set -o errexit
 
-# Instalar as dependências do Python
 pip install -r requirements.txt
 
-# Criar as tabelas do banco de dados
-# (Isso executará o comando db.create_all() no contexto da sua aplicação)
-flask shell <<< "from app import db; db.create_all()"
-
-echo "Build finalizado com sucesso!"
+python -c "from app import app, db; app.app_context().push(); db.create_all()"
